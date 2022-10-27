494ef3c02ae32039d7ca49cb806ac6af0ff3e230
root <root@debian-BULLSEYE-live-builder-AMD64>
2022-10-27 20:31:57 -0300

    Commit do amor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import monkey from './urso.jpeg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={monkey} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {`Perdoa o guilherme`}
         </p>
         <a
           className="App-link"
